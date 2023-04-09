bbf10d68364aa345dfd0d7b5ff9e7fc97617407a
MrCream0 <slamboi666@gmail.com>
2023-04-09 10:54:54 -0500

    heroku stopped working this commit is trying vercel as a host

--- jamming/src/util/Spotify.js
+++ jamming/src/util/Spotify.js
@@ -1,5 +1,5 @@
 const clientId = "82455fcc59e24efd9cedc98ec2b8ddb4";
-const redirectUri = "https://spotlist.herokuapp.com/callback";
+const redirectUri = "https://spotify-playlist-two.vercel.app/";
 let accessToken;
 
 const Spotify = {
